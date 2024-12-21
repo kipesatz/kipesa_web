79c6b9f05459bdd347c454d8a28d5d728d016a16
Alex Urassa <alexurassa03@gmail.com>
2024-12-21 12:15:29 +0300

    create lib with entry-point `@kps/data`

---
+++ libs/feats/accounts/auth/src/lib/pages/login-page/login-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'kps-login-page',
+  imports: [CommonModule],
+  templateUrl: './login-page.component.html',
+  styleUrl: './login-page.component.scss',
+})
+export class LoginPageComponent {}
---
+++ libs/shared/data/auth/README.md
@@ -0,0 +1,3 @@
+# @kps/data/auth
+
+Secondary entry point of `@kps/data`. It can be used by importing from `@kps/data/auth`.
