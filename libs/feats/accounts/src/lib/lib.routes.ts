7004f13f5c34875a3d85cec0edbdf9029ecf1b66
Alex Urassa <alexurassa03@gmail.com>
2024-12-21 11:45:35 +0300

    create lib `@kps/accounts`

---
+++ libs/feats/accounts/src/lib/lib.routes.ts
@@ -0,0 +1,6 @@
+import { Route } from '@angular/router';
+import { AccountsComponent } from './accounts/accounts.component';
+
+export const accountsRoutes: Route[] = [
+  { path: '', component: AccountsComponent },
+];
