1f32fc58fd5a8ac997697a67cf7a94d65a143f47
Alex Urassa <alexurassa03@gmail.com>
2024-12-22 11:12:32 +0300

    create entry-point `@kps/material/table`

---
+++ libs/shared/material/table/src/lib/data-table/data-table.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'kps-data-table',
+  imports: [CommonModule],
+  templateUrl: './data-table.component.html',
+  styleUrl: './data-table.component.scss',
+})
+export class DataTableComponent {}
