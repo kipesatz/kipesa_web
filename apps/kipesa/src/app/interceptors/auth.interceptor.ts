210cf70b319d70f1442b16edc9d608ecce4844b6
Alex Urassa <alexurassa03@gmail.com>
2024-12-31 17:46:28 +0300

    create auth.interceptor for injecting jwt-token

---
+++ apps/kipesa/src/app/interceptors/auth.interceptor.ts
@@ -0,0 +1,20 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { AuthTokens } from '@kps/data/auth';
+import { JWT_AUTH_TOKEN_KEY, StorageService } from '@kps/data/storage';
+
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const jwtTokenKey = inject(JWT_AUTH_TOKEN_KEY);
+  const storageService = inject(StorageService<AuthTokens>);
+
+  const tokens: AuthTokens | null = storageService.getItem(jwtTokenKey);
+  if (!tokens?.accessToken) {
+    return next(req);
+  }
+
+  const authReq = req.clone({
+    headers: req.headers.set('Authorization', `Bearer ${tokens.accessToken}`),
+  });
+
+  return next(authReq);
+};
