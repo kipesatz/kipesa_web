e891a6c579e16208a0990df4c27efd5e65b16008
Alex Urassa <alexurassa03@gmail.com>
2024-12-31 17:47:09 +0300

    Add default configs in app.config & add more root routes

--- apps/kipesa/src/app/app.routes.ts
+++ apps/kipesa/src/app/app.routes.ts
@@ -1,10 +1,47 @@
+import { importProvidersFrom } from '@angular/core';
 import { Route } from '@angular/router';
+import { MyMembershipDataModule } from '@kps/data/associations';
+import { LoginDataModule } from '@kps/data/auth';
+import { MainLayoutComponent } from '@kps/layout';
 
 export const appRoutes: Route[] = [
   {
-    path: 'accounts',
-    title: 'Accounts',
-    loadChildren: () => import('@kps/accounts').then((m) => m.accountsRoutes),
+    path: '',
+    providers: [importProvidersFrom(LoginDataModule, MyMembershipDataModule)],
+    children: [
+      {
+        path: 'accounts',
+        title: 'Accounts',
+        loadChildren: () =>
+          import('@kps/accounts').then((m) => m.accountsRoutes),
+      },
+      {
+        path: '',
+        component: MainLayoutComponent,
+        children: [
+          {
+            path: 'dashboards',
+            loadChildren: () =>
+              import('./dashboards').then((m) => m.dashboardRoutes),
+          },
+          {
+            path: 'finances',
+            title: 'Finances',
+            loadChildren: () =>
+              import('@kps/finances').then((m) => m.financesRoutes),
+          },
+          {
+            path: 'associations',
+            title: 'Associations',
+            loadChildren: () =>
+              import('@kps/associations').then((m) => m.associationsRoutes),
+          },
+          { path: '**', redirectTo: 'dashboards' },
+        ],
+      },
+      { path: '**', redirectTo: 'accounts' },
+    ],
   },
-  { path: '**', redirectTo: 'accounts' },
+  { path: '', redirectTo: 'accounts', pathMatch: 'full' },
+  { path: '**', redirectTo: 'accounts', pathMatch: 'full' },
 ];
