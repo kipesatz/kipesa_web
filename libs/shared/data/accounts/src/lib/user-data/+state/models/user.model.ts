7274194c7278f419218772cd923ab684c4958e86
Alex Urassa <alexurassa03@gmail.com>
2024-12-31 18:00:48 +0300

    npx -> create an entry-point `@kps/data/accounts` with user-model

---
+++ libs/shared/data/accounts/src/lib/user-data/+state/models/user.model.ts
@@ -0,0 +1,10 @@
+export interface User {
+  id: string;
+  firstName: string;
+  middleName: string | null;
+  lastName: string;
+  email: string;
+  phoneNumber: string;
+  createdOn: string;
+  lastEditedOn: string;
+}
