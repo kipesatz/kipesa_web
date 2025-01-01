a8dbd8411741a1d46f79910cc0ecac6723d44675
Alex Urassa <alexurassa03@gmail.com>
2025-01-01 22:39:07 +0300

    nx -> create @angular lib `@kps/admin`

---
+++ libs/feats/admin/src/lib/admin/admin.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'kps-admin',
+  imports: [CommonModule],
+  templateUrl: './admin.component.html',
+  styleUrl: './admin.component.scss',
+})
+export class AdminComponent {}
