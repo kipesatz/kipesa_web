751c29621612dabf265bdc5c4c244dc03d8f5e2a
Alex Urassa <alexurassa03@gmail.com>
2025-01-01 22:42:09 +0300

    nx -> create an entry-point `@kps/admin/settings` & app.config

---
+++ libs/feats/admin/settings/README.md
@@ -0,0 +1,3 @@
+# @kps/admin/settings
+
+Secondary entry point of `@kps/admin`. It can be used by importing from `@kps/admin/settings`.
--- libs/feats/admin/src/lib/lib.routes.ts
+++ libs/feats/admin/src/lib/lib.routes.ts
@@ -1,4 +1,12 @@
 import { Route } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
 
-export const adminRoutes: Route[] = [{ path: '', component: AdminComponent }];
+export const adminRoutes: Route[] = [
+  { path: '', component: AdminComponent },
+  {
+    path: 'settings',
+    title: 'Settings',
+    loadChildren: () =>
+      import('@kps/admin/settings').then((lib) => lib.adminSettingsRoutes),
+  },
+];
