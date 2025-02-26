8b1e015eb9cd8354f9914afb63128c686493396c
alexurassa <alexurassa03@gmail.com>
2025-02-26 16:15:01 +0300

    Format login-form & Add logic for login the user

--- libs/feats/accounts/auth-forms/src/lib/login/components/login-form/login-form.component.ts
+++ libs/feats/accounts/auth-forms/src/lib/login/components/login-form/login-form.component.ts
@@ -1,13 +1,18 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, output } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
-import { EmailFieldComponent, PasswordFieldComponent, PhoneFieldComponent } from '@kps/forms/fields';
+import {
+  EmailFieldComponent,
+  PasswordFieldComponent,
+  PhoneFieldComponent,
+} from '@kps/forms/fields';
 import {
   MatButtonToggle,
   MatButtonToggleGroup,
 } from '@angular/material/button-toggle';
-import { LoginFormGroup, LoginFormService } from '../../services';
+import { LoginFg, LoginFormService } from '../../services';
 import { BaseFormComponent } from '@kps/forms';
 import { ButtonComponent } from '@kps/material/button';
+import { LoginFacadeService, LoginPayload } from '@kps/data/auth';
 @Component({
   selector: 'kps-login-form',
   imports: [
@@ -17,24 +22,31 @@ import { ButtonComponent } from '@kps/material/button';
     PhoneFieldComponent,
     EmailFieldComponent,
     PasswordFieldComponent,
-    ButtonComponent
+    ButtonComponent,
   ],
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.scss',
 })
 export class LoginFormComponent extends BaseFormComponent implements OnInit {
   private loginFormService = inject(LoginFormService);
-  public hidePassword = true;
+  private loginFacade = inject(LoginFacadeService)
 
+  // outputs
+  loginChange = output<LoginPayload>();
+
+  loading$ = this.loginFacade.loading
+  public hidePassword = true;
   public loginMethodControl = new FormControl<'email' | 'phone'>('phone');
 
-  public getFormGroup(): LoginFormGroup {
-    return this.loginFormService.loginForm;
+  getFormGroup = (): LoginFg => this.loginFormService.loginForm();
+
+  submitForm(): void {
+    if (this.getFormGroup().valid) {
+      this.loginChange.emit(this.getFormGroup().value as LoginPayload);
+    }
   }
 
   ngOnInit(): void {
-    this.loginFormService.initForm();
-
     // Reset form when login method changes
     this.loginMethodControl.valueChanges.subscribe(() => {
       this.getFormGroup().get('loginId')?.setValue('');
--- libs/feats/accounts/auth-forms/src/lib/login/services/login-form.service.ts
+++ libs/feats/accounts/auth-forms/src/lib/login/services/login-form.service.ts
@@ -1,9 +1,14 @@
-import { inject, Injectable } from '@angular/core';
-import { FormControl, FormGroup, NonNullableFormBuilder, Validators } from '@angular/forms';
+import { inject, Injectable, signal } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  NonNullableFormBuilder,
+  Validators,
+} from '@angular/forms';
 import { LoginPayload } from '@kps/data/auth';
 import { BaseFormService } from '@kps/forms';
 
-export type LoginFormGroup = FormGroup<{
+export type LoginFg = FormGroup<{
   [K in keyof LoginPayload]: FormControl<LoginPayload[K]>;
 }>;
 
@@ -14,13 +19,9 @@ export class LoginFormService extends BaseFormService<LoginPayload> {
   protected formBuilder: NonNullableFormBuilder = inject(
     NonNullableFormBuilder
   );
-  loginForm!: LoginFormGroup;
+  loginForm = signal<LoginFg>(this.buildForm());
 
-  public initForm(): void {
-    this.loginForm = this.buildForm();
-  }
-
-  protected override buildForm(): FormGroup {
+  public override buildForm(): FormGroup {
     return this.formBuilder.group({
       loginId: [
         '',
