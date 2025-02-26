b3d8fe3c654855815d5ef1bb2391d11e860de8f6
alexurassa <alexurassa03@gmail.com>
2025-02-26 16:20:39 +0300

    create my-account container comp

---
+++ libs/feats/accounts/my-account/src/lib/my-account-container/my-account-container.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { MatTabLink, MatTabNav, MatTabNavPanel } from '@angular/material/tabs';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { MatIcon } from '@angular/material/icon';
+
+@Component({
+  selector: 'kps-my-account-container',
+  imports: [
+    MatTabNav,
+    MatTabNavPanel,
+    MatTabLink,
+    RouterLinkActive,
+    RouterLink,
+    RouterOutlet,
+    MatIcon,
+  ],
+  templateUrl: './my-account-container.component.html',
+  styleUrl: './my-account-container.component.scss',
+})
+export class MyAccountContainerComponent {
+  links = [
+    { path: 'personalInfo', label: 'Personal Info', icon: 'person' },
+    { path: 'enrollments', label: 'Enrolled Associations', icon: 'groups' },
+    {
+      path: 'contributions',
+      label: 'My Contributions',
+      icon: 'volunteer_activism',
+    },
+    { path: 'depts', label: 'My Depts', icon: 'account_balance' },
+  ];
+}
