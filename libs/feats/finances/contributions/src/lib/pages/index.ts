b5d0e6d77e1329bacb2818233fbf5aa7f01205e1
alexurassa <alexurassa03@gmail.com>
2025-02-26 16:40:17 +0300

    Add cp-details container cmp & cp-details component

---
+++ libs/feats/finances/contributions/src/lib/pages/index.ts
@@ -0,0 +1,11 @@
+export * from './cps-page/cps-page.component';
+
+export * from './contributions-page/contributions-page.component';
+
+export * from './cp-creation-dialog/cp-creation-dialog.component';
+
+export * from './cp-details/cp-details.component';
+
+export * from './cp-contributions/cp-contributions.component';
+
+export * from './cp-details-container/cp-details-container.component';
