5c806a155ffbbc77bb399948927501199c0d3c0b
alexurassa <alexurassa03@gmail.com>
2025-02-26 12:27:11 +0300

    forms: Remove functional parts from base-search-field to search-field

--- libs/shared/forms/fields/src/lib/search-field/base-search-field.component.ts
+++ libs/shared/forms/fields/src/lib/search-field/base-search-field.component.ts
@@ -1,21 +1,4 @@
-import {
-  Component,
-  effect,
-  inject,
-  input,
-  InputSignal,
-  model,
-  ModelSignal,
-  OnInit,
-} from '@angular/core';
-import { toSignal } from '@angular/core/rxjs-interop';
-import {
-  NonNullableFormBuilder,
-  FormControl,
-  Validators,
-} from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
-import { debounceTime } from 'rxjs';
+import { Component, input, InputSignal } from '@angular/core';
 
 @Component({
   selector: 'kps-base-search-field',
@@ -23,37 +6,9 @@ import { debounceTime } from 'rxjs';
   template: ``,
   styles: ``,
 })
-export class BaseSearchFieldComponent implements OnInit {
-  constructor() {
-    effect(() => {
-      this.searchTerm.set(this._searchTerm() ?? '');
-    });
-  }
-  // deps
-  protected currentRoute = inject(ActivatedRoute);
-  private controllerBuilder = inject(NonNullableFormBuilder);
-  // props
-  protected searchController!: FormControl<string>;
-  protected qStringValue = this.currentRoute.snapshot.queryParamMap.get('q');
-
-  /**Release the latest search value from search input after each milliseconds specified in `searchDelay` */
-  searchTerm: ModelSignal<string> = model<string>(
-    this.currentRoute.snapshot.queryParamMap.get('q') ?? ''
-  );
-
+export class BaseSearchFieldComponent {
   // signals
   searchPlaceholder: InputSignal<string> = input<string>('Search here...');
   /**Duration in milliseconds to delay before detecting changes in the search input */
   searchDelay: InputSignal<number> = input<number>(500);
-
-  private _searchTerm = toSignal(
-    this.searchController.valueChanges.pipe(debounceTime(this.searchDelay()))
-  );
-
-  ngOnInit(): void {
-    // init search controller
-    this.searchController = this.controllerBuilder.control(this.searchTerm(), {
-      validators: [Validators.maxLength(400)],
-    });
-  }
 }
--- libs/shared/forms/fields/src/lib/search-field/search-field.component.ts
+++ libs/shared/forms/fields/src/lib/search-field/search-field.component.ts
@@ -1,4 +1,14 @@
-import { Component, input, InputSignal, output, signal } from '@angular/core';
+import {
+  Component,
+  effect,
+  inject,
+  input,
+  InputSignal,
+  model,
+  ModelSignal,
+  output,
+  signal,
+} from '@angular/core';
 import {
   MatFormField,
   MatPrefix,
@@ -10,6 +20,10 @@ import { MatInput } from '@angular/material/input';
 import { MatMenu, MatMenuTrigger } from '@angular/material/menu';
 import { MatListOption, MatSelectionList } from '@angular/material/list';
 import { TableFilter } from '@kps/material/table';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { debounceTime, distinctUntilChanged } from 'rxjs';
+import { FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'kps-search-field',
@@ -24,15 +38,48 @@ import { TableFilter } from '@kps/material/table';
 
     MatSelectionList,
     MatListOption,
+    ReactiveFormsModule,
   ],
   templateUrl: './search-field.component.html',
   styleUrl: './search-field.component.scss',
 })
 export class SearchFieldComponent extends BaseSearchFieldComponent {
+  constructor() {
+    super();
+
+    effect(() => {
+      if (this._searchString()) {
+        this.searchTerm.set(this.searchController.value);
+      }
+    });
+  }
+  // deps
+  protected curRoute = inject(ActivatedRoute);
+
   availableFilters: InputSignal<TableFilter[] | undefined> =
     input<TableFilter[]>();
   activeFilters = signal<Record<string, unknown>>({});
   filterChange = output<Record<string, unknown>>();
+  /**Release the latest search value from search input after each milliseconds specified in `searchDelay` */
+  searchTerm: ModelSignal<string> = model<string>(
+    this.curRoute.snapshot.queryParamMap.get('q') ?? ''
+  );
+
+  // form controller
+  searchController: FormControl<string> = new FormControl<string>(
+    this.searchTerm(),
+    {
+      validators: [Validators.maxLength(400)],
+      nonNullable: true,
+    }
+  );
+  // mark as dirty after every `searchDelay() and value has changed compared to the prev one`
+  private _searchString = toSignal(
+    this.searchController.valueChanges.pipe(
+      debounceTime(this.searchDelay()),
+      distinctUntilChanged()
+    )
+  );
 
   // Filter methods
   toggleFilter(filter: TableFilter, value: unknown) {
