bdc7ff4511aed25b84dd7151472e1d6bdc7dbc1f
alexurassa <alexurassa03@gmail.com>
2025-02-26 16:28:16 +0300

    Add admin setting routes

--- libs/feats/admin/settings/src/lib/admin-settings.routes.ts
+++ libs/feats/admin/settings/src/lib/admin-settings.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const adminSettingsRoutes: Routes = [];
+export const adminSettingsRoutes: Routes = [
+  {
+    path: 'settings',
+    loadChildren: () =>
+      import('./admin-settings/admini-settings.routes').then(
+        (m) => m.adminSettingsRoutes
+      ),
+  },
+  { path: '**', redirectTo: 'settings', pathMatch: 'full' },
+];
