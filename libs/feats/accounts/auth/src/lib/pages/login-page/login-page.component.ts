f2252e7cc8bf8b1493d389067c9c88cd60b1a1aa
alexurassa <alexurassa03@gmail.com>
2025-02-26 16:17:56 +0300

    Add ui logic for login in login page

--- libs/feats/accounts/auth/src/lib/pages/login-page/login-page.component.ts
+++ libs/feats/accounts/auth/src/lib/pages/login-page/login-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { LoginFormComponent } from '@kps/accounts/auth-forms';
 import {
   MatCard,
@@ -6,7 +6,13 @@ import {
   MatCardContent,
   MatCardMdImage,
 } from '@angular/material/card';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import {
+  AuthCheckService,
+  LoginFacadeService,
+  LoginPayload,
+} from '@kps/data/auth';
+
 @Component({
   selector: 'kps-login-page',
   imports: [
@@ -20,4 +26,19 @@ import { RouterLink } from '@angular/router';
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss',
 })
-export class LoginPageComponent {}
+export class LoginPageComponent implements OnInit {
+  loginFacadeService = inject(LoginFacadeService);
+  private router = inject(Router);
+  private authCheckService = inject(AuthCheckService);
+
+  dispatchLoginAttempt(loginPayload: LoginPayload): void {
+    this.loginFacadeService.dispatchLogin(loginPayload);
+  }
+
+  ngOnInit(): void {
+    // check if authenticated
+    if (this.authCheckService.isAuthenticated()) {
+      this.router.navigateByUrl('/dashboards/myDashboard');
+    }
+  }
+}
