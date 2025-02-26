dfedde3204c9b87f6113d131d10e2a6a823b2946
alexurassa <alexurassa03@gmail.com>
2025-02-26 16:39:16 +0300

    Add contribution purposes page

---
+++ libs/feats/finances/contributions/src/lib/pages/cps-page/cps-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CpsTableComponent } from '../../components';
+
+@Component({
+  selector: 'kps-cps-page',
+  imports: [CpsTableComponent],
+  templateUrl: './cps-page.component.html',
+  styles: ``,
+})
+export class CpsPageComponent {}
