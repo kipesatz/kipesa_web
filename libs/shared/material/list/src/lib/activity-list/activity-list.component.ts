540d3121ecee5f3673fe2b1a76746d6fc2ea922a
alexurassa <alexurassa03@gmail.com>
2025-03-04 02:18:32 +0300

    npx: create ng-entry-point `@kps/material/list` with activity-list cmp

---
+++ libs/shared/material/list/src/lib/activity-list/activity-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'kps-activity-list',
+  imports: [CommonModule],
+  templateUrl: './activity-list.component.html',
+  styleUrl: './activity-list.component.scss',
+})
+export class ActivityListComponent {}
