4b6c12a13bfa130c3586bca12227069cfe09e4ac
alexurassa <alexurassa03@gmail.com>
2025-03-03 23:52:50 +0300

    create ng entry-point `@kps/material/card`

---
+++ libs/shared/material/card/src/lib/stat-card/stat-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'kps-stat-card',
+  imports: [CommonModule],
+  templateUrl: './stat-card.component.html',
+  styleUrl: './stat-card.component.scss',
+})
+export class StatCardComponent {}
