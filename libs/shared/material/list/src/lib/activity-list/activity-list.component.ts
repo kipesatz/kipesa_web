395c218c19ab35a976201b1ac21d6a8c137c3658
alexurassa <alexurassa03@gmail.com>
2025-03-04 14:20:35 +0300

    Implement the activity list component

--- libs/shared/material/list/src/lib/activity-list/activity-list.component.ts
+++ libs/shared/material/list/src/lib/activity-list/activity-list.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { DatePipe } from '@angular/common';
+import { Component, input } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
+import {
+  MatList,
+  MatListItem,
+  MatListItemIcon,
+  MatListItemLine,
+  MatListItemTitle,
+} from '@angular/material/list';
+import { LoadingIndicatorComponent } from '@kps/material/progress';
+
+export interface Activity {
+  type: string;
+  description: string;
+  timestamp: Date;
+}
 
 @Component({
   selector: 'kps-activity-list',
-  imports: [CommonModule],
+  imports: [
+    MatList,
+    MatListItem,
+    MatListItemIcon,
+    MatListItemTitle,
+    MatListItemLine,
+    MatIcon,
+    DatePipe,
+    LoadingIndicatorComponent,
+  ],
   templateUrl: './activity-list.component.html',
   styleUrl: './activity-list.component.scss',
 })
-export class ActivityListComponent {}
+export class ActivityListComponent {
+  activities = input<Activity[]>([]);
+  loading = input<boolean>(false);
+
+  getIconForType(type: string): string {
+    switch (type.toLowerCase()) {
+      case 'loan':
+        return 'money';
+      case 'member':
+        return 'person';
+      case 'payment':
+        return 'payment';
+      default:
+        return 'info';
+    }
+  }
+}
